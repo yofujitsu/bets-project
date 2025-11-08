8abce903ad018a7fa8f076b1ab8e1293dc581093
yofujitsu <yofujitsuuu@gmail.com>
2025-11-08 16:15:26 +0300

    main: fix cors todo: highlight nominees

--- src/main/java/com/csbets/vcsbets/config/SecurityConfig.java
+++ src/main/java/com/csbets/vcsbets/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://vitejs-vite-duplicat-qz9u.bolt.host"));
+        config.setAllowedOrigins(List.of("https://vitejs-vite-duplicat-qz9u.bolt.host", "https://veronika-duo-cup-lan-kbe3.bolt.host"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         source.registerCorsConfiguration("/**", config);
